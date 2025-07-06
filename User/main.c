13a9e6f2fcabd34b6c379443d7ce0e956aa8cb00
myrockey <2771452556@qq.com>
2025-07-07 07:16:11 +0800

    蓝牙模块从模式

--- User/main.c
+++ User/main.c
@@ -1,13 +1,44 @@
 #include "stm32f10x.h"                  // Device header
 #include "SmartCar.h"
+#include "Serial.h"
+
+uint8_t RxData;
 
 int main(void)
 {
 	SmartCar_Init();
-	Clockwise_Rotation();
+	Serial_Init();
+	
 	while(1)
 	{
-		
+		if(Serial_GetRxFlag() == 1)
+		{
+			RxData = Serial_GetRxData();
+		}
+		 
+		switch(RxData)
+		{
+			case 1:
+				Move_Forward();
+				break;
+			case 2:
+				Move_Backward();
+				break;
+			case 3:
+				Car_Stop();
+				break;
+			case 4:
+				Turn_Left();
+				break;
+			case 5:
+				Turn_Right();
+				break;
+			case 6:
+				Clockwise_Rotation();
+				break;
+			case 7:
+				CounterClockwise_Rotation();
+				break;
+		}
 	}
-	
 }
