6348f1914e57b5f568d005ae9f1a3fae8d88921f
luoqi <2771452556@qq.com>
2025-07-08 10:29:13 +0800

    调整引脚

--- Hardware/tracking.h
+++ Hardware/tracking.h
@@ -4,9 +4,9 @@
 #include "stm32f10x.h"                  // Device header
 
 //三路循迹
-#define   R   GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_4)
-#define   M    GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_5) 
-#define   L   GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_6) 
+#define   R   GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3)
+#define   M    GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_4) 
+#define   L   GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_5) 
 
 void Tracking_Init(void);
 void Tracking_Run(void);
--- README.md
+++ README.md
@@ -8,6 +8,6 @@ PA4 PA5 PA6 PA7 Motor
 PA1 PA2 PA11    PWM(TIM2,TIM1)
 PA9 PA10        Serial(USART1)
 
-PB8 PB9         OLED
-PB4 PB5 PB6     Tracking
+PB6 PB7 PB8 PB9 OLED
+PB3 PB4 PB5     Tracking
 PB0 PB1         Ultrasonic(TIM3)
