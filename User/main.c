b299467480b6462c5a274574d3dcd9264fdbfcad
myrockey <2771452556@qq.com>
2025-07-09 07:15:50 +0800

    fix

--- User/main.c
+++ User/main.c
@@ -6,10 +6,10 @@
 #include "Serial.h"
 #include "Bluetooth.h"
 #include "Ultrasonic.h"
-#include "tracking.h"
-#include "Buzzer.h"
+// #include "tracking.h"
+// #include "Buzzer.h"
 #include "LED.h"
-#include "VoiceIdentify.h"
+// #include "VoiceIdentify.h"
 
 uint8_t RxData;//串口接收数据的变量
 int tmp;//记录循迹位置值的变量
@@ -31,11 +31,11 @@ int main(void)
 
 	while(1)
 	{
-		tmp = (L * 100)+ (M * 10) + (R * 1);
+		// tmp = (L * 100)+ (M * 10) + (R * 1);
 		distance = Ultrasonic_Distance();
 
         OLED_ShowNum(2,4,distance,3);
-        OLED_ShowNum(3,4,tmp,3);
+        // OLED_ShowNum(3,4,tmp,3);
 		
 		// 接收到数据
 		if(Serial_GetRxFlag() == 1)
@@ -46,19 +46,19 @@ int main(void)
 		Exec_Function(RxData, str);
 		OLED_ShowString(1,4,str);
 
-		Voice_broadcast(RxData);
+		//Voice_broadcast(RxData);
 		
 		//距离太近时
-		if(distance < 10)
-        {
-            Buzzer_OFF;
-            LED1_OFF;
-        }
-        if(distance > 10)
-        {
-            Buzzer_ON;
-            LED1_ON;
-        }
+		// if(distance < 10)
+        // {
+        //     Buzzer_OFF;
+        //     LED1_OFF;
+        // }
+        // if(distance > 10)
+        // {
+        //     Buzzer_ON;
+        //     LED1_ON;
+        // }
 
 	}
 }
@@ -70,10 +70,10 @@ void BSP_Init(void)
 	SmartCar_Init();//电机驱动初始化
 	Bluetooth_Init();//蓝牙初始化
 	Ultrasonic_Init();//超声波初始化
-	Tracking_Init();//循迹初始化
-	Buzzer_Init();//蜂鸣器初始化
+	// Tracking_Init();//循迹初始化
+	// Buzzer_Init();//蜂鸣器初始化
 	LED_Init();//LED初始化
-	VoiceIdentify_Init();//语音识别初始化
+	// VoiceIdentify_Init();//语音识别初始化
 }
 
 /*
@@ -113,13 +113,13 @@ void Exec_Function(uint8_t type, char str[])
 			strcpy(str, " Ncycle ");
 			break;
 		case 8://超声波避障
-			Ultrasonic_Run();
-			strcpy(str, " sonic  ");
-			break;
-		case 9://循迹
-			Tracking_Run();
-			strcpy(str, " sonic  ");
-			break;
+		 	Ultrasonic_Run();
+		 	strcpy(str, " sonic  ");
+		 	break;
+		// case 9://循迹
+		// 	Tracking_Run();
+		// 	strcpy(str, " sonic  ");
+		// 	break;
 		case 10://LED ON
 			LED1_ON;
 			//LED2_ON;LED3_ON;
@@ -130,14 +130,14 @@ void Exec_Function(uint8_t type, char str[])
 			//LED2_OFF;LED3_OFF;
 			strcpy(str, " led off ");
 			break;
-		case 12://Buzzer ON
-			Buzzer_ON;
-			strcpy(str, " buzzer ");
-			break;
-		case 13://Buzzer OFF
-			Buzzer_OFF;
-			strcpy(str, " buzzer ");
-			break;
+		// case 12://Buzzer ON
+		// 	Buzzer_ON;
+		// 	strcpy(str, " buzzer ");
+		// 	break;
+		// case 13://Buzzer OFF
+		// 	Buzzer_OFF;
+		// 	strcpy(str, " buzzer ");
+		// 	break;
 	}
 }
 
@@ -145,15 +145,15 @@ void Exec_Function(uint8_t type, char str[])
 *根据参数，播报语音。
 具体原理：stm32（发送方）通过串口传输数据 语音识别模块ASRPRO（接收方），根据接收的数据，执行语音播报。
 */
-void Voice_broadcast(uint8_t type)
-{
-	switch(type)
-	{
-		case 10://LED ON
-			VoiceIdentify_SendString("LED ON");
-			break;
-		case 11://LED OFF
-			VoiceIdentify_SendString("LED OFF");
-			break;
-	}
-}
+//void Voice_broadcast(uint8_t type)
+//{
+//	switch(type)
+//	{
+//		case 10://LED ON
+//			VoiceIdentify_SendString("LED ON");
+//			break;
+//		case 11://LED OFF
+//			VoiceIdentify_SendString("LED OFF");
+//			break;
+//	}
+//}
